a6abadcd175909d932fe72b333f45091a6e36575
UmemaShaikh <163534115+UmemaShaikh@users.noreply.github.com>
2024-06-29 23:15:38 +0530

    Add files via upload

---
+++ BMI generator.js
@@ -0,0 +1,26 @@
+let form=document.querySelector('form');
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    const height=parseInt(document.querySelector('#height').value);
+    const  weight=parseInt(document.querySelector('#weight').value);
+    let result=document.querySelector('#result');
+
+    if(height ==="" || height<0 || isNaN(height)){
+        result.innerHTML=`Please give a valid height ${height}`
+    }
+     else if(weight ==="" || weight<0 || isNaN(weight)){
+        result.innerHTML=`Please give a valid weight ${weight}`
+    }
+   else{
+    const bmi= (weight/((height*height)/10000)).toFixed(2);
+    result.innerHTML=`<br><span>${bmi}</span>`;
+
+    if(bmi<18.5){
+       result.innerHTML+=` You are underweight`;
+    }else if(bmi>18.6 && bmi<24.9 ){
+       result.innerHTML+=` You are normal weight`;
+    }else{
+     result.innerHTML+=` You are overweight`;
+    } 
+    }   
+}) 
